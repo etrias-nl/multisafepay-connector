dfa847a81c113b5a5be419b67d48207702c63948
Christ-Jan <christ-jan@etrias.nl>
2019-07-22 11:23:09 +0200

    WIP

---
+++ tests/Functional/Services/OrderServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+
+class OrderServiceTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testCreateOrder(): void
+    {
+        var_dump('blaat');
+    }
+}
